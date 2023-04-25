8c5ce82f571d7a8c3010b89daa8c70bb3ae736be
MunJitso <72764494+MunJitso@users.noreply.github.com>
2023-04-25 17:20:45 +0000

    Update repos.component.ts

--- src/app/repos/repos.component.ts
+++ src/app/repos/repos.component.ts
@@ -1,40 +1,63 @@
-import { Component } from '@angular/core';
-import { ApiService } from './api.service';
-import { faJava, faPython, faJs, faHtml5, faCss3, faRust, IconDefinition } from '@fortawesome/free-brands-svg-icons';
-import { Router } from '@angular/router';
+import { Component } from "@angular/core";
+import { ApiService } from "./api.service";
+import {
+  faJava,
+  faPython,
+  faJs,
+  faHtml5,
+  faCss3,
+  faRust,
+  IconDefinition,
+} from "@fortawesome/free-brands-svg-icons";
+import { Router } from "@angular/router";
 
-interface LanguageIcons{
-  [key : string] : IconDefinition
+interface LanguageIcons {
+  [key: string]: IconDefinition;
 }
 interface Repo {
-  id : number;
-  name : string;
+  id: number;
+  name: string;
   description: string;
-  html_url:string;
-  language:string;
+  html_url: string;
+  language: string;
 }
 @Component({
-  selector: 'app-repos',
-  templateUrl: './repos.component.html',
-  styleUrls: ['./repos.component.css']
+  selector: "app-repos",
+  templateUrl: "./repos.component.html",
+  styleUrls: ["./repos.component.css"],
 })
 export class ReposComponent {
-  languageIcons : LanguageIcons = {
-    "Java" : faJava,
-    "HTML" : faHtml5,
-    "CSS" : faCss3,
-    "Python" : faPython,
-    "Rust" : faRust,
-    "JavaScript" : faJs,
-    "TypeScript" : faJs
-  }
-  
-  constructor(private apiService: ApiService, public router : Router){}
-  reposList : Repo[] = [];
-  names = [];
+  languageIcons: LanguageIcons = {
+    Java: faJava,
+    HTML: faHtml5,
+    CSS: faCss3,
+    Python: faPython,
+    Rust: faRust,
+    JavaScript: faJs,
+    TypeScript: faJs,
+  };
+
+  constructor(private apiService: ApiService, public router: Router) {}
+
+  searchQuery: string = "";
+  reposList: Repo[] = [];
+  reposUpdated: Repo[] = [];
+
   ngOnInit(): void {
-    this.apiService.getRepos().subscribe(repos => {
+    this.apiService.getRepos().subscribe((repos) => {
       this.reposList = repos;
+      this.reposUpdated = repos;
     });
   }
-} 
+
+  onSearch() {
+    const filteredRepos = this.reposList.filter((repo) => {
+      let searchQuery = this.searchQuery;
+      if (repo.name.toLowerCase().includes(searchQuery.toLowerCase())) {
+        return true;
+      }
+      return false;
+    });
+    this.reposUpdated = filteredRepos;
+  }
+}
