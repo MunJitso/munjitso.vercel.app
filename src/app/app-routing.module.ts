0420987272302f6b8342ca2a93c52191e5b49f5c
MunJitso <72764494+MunJitso@users.noreply.github.com>
2023-04-25 01:31:57 +0100

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ReposComponent } from './repos/repos.component';
+import { SkillsComponent } from './skills/skills.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent},
   { path: 'repos', component: ReposComponent},
+  { path: 'skills', component: SkillsComponent}
 ];
 
 @NgModule({
